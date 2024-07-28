315c6f4b60163e76db790b3649a60e983a4a2daa
Ugor Marcilio Brandão Costa <ugor_gost@hotmail.com>
2024-07-28 00:09:36 -0300

    refact: App.js

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
-import { database } from './src/db';
-import AppNavigator from './AppNavigator'; // ajuste o caminho conforme necessário
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import AppNavigator from './AppNavigator'; 
 
-
-
-
-export default function App() {
-  return <AppNavigator />;
-}
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <AppNavigator />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F2F1F7',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
-AppRegistry.registerComponent('GEROCuidado', () => App);
+export default App;
