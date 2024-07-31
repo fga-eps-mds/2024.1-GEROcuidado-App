fa3ad7db3c667585bcaedd39cada0c3c60926971
Gustavo Abrantes <gugaabrantes2015@gmail.com>
2024-07-30 21:13:08 -0300

    style: adding the routine header

--- src/screens/tabBar/Rotina.js
+++ src/screens/tabBar/Rotina.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const Rotina = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Rotina na pasta tabBar</Text>
+      <View style={styles.header}>
+        <View style={styles.imageContainer}>
+          <Image source={require('../../../assets/elders/elder_1.png')} style={styles.logo_caregiver} />
+          <Text style={styles.helloMessage}>Cuidando de nome do idoso</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -17,10 +22,30 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: '#f5f5f5',
   },
-  title: {
-    fontSize: 24,
+  header: {
+    height: '23%',
+    width: '110%',
+    alignItems: 'center',
+    paddingLeft: 20,
+    marginTop: '-200%',
+    backgroundColor: "#2CCDB5",
+  },
+  imageContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginRight: '7%',
+  },
+  logo_caregiver: {
+    width: 60,
+    height: 60,
+    marginTop: 63,
+    borderRadius: 30,
+  },
+  helloMessage: {
+    fontSize: 18,
+    color: 'white',
+    marginTop: 8,
     fontWeight: 'bold',
-    marginBottom: 20,
   },
 });
 
