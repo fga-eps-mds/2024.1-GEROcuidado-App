a497a594f8d3c73ab73d8608b321bdf32ac84c95
Gustavo Abrantes <gugaabrantes2015@gmail.com>
2024-08-02 17:51:56 -0300

    feat: changing the variables types

--- src/model/rotina.js
+++ src/model/rotina.js
@@ -2,15 +2,15 @@ import { Model } from '@nozbe/watermelondb'
 import { field, text, date, relation } from '@nozbe/watermelondb/decorators'
 
 export default class Rotina extends Model {
-  static table = 'rotina'
+  static table = 'rotina';
 
   @text('titulo') titulo
   @date('dataHora') dataHora
   @field('categoria') categoria
   @text('dias') dias
-  @text('descricao') descricao
+  @field('descricao') descricao
   @text('token') token
   @text('notificacao') notificacao
-  @text('dataHoraConcluída') dataHoraConcluída
+  @date('dataHoraConcluída') dataHoraConcluída
   @relation('idoso', 'idoso_id') idoso
 }
